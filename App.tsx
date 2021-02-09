c816c0cc8bf3444169a69b833398ec8fcaca1e70
thegouge <thegouge42@gmail.com>
2021-02-09 13:25:27 -0800

    made the app a tab-navigation

--- App.tsx
+++ App.tsx
@@ -1,8 +1,10 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
 import React from 'react';
 import { StyleSheet } from 'react-native';
 import { ThemeProvider } from 'react-native-elements';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 import { GroceryContainer } from './src/pages/GroceryContainer';
 import { PantryContainer } from './src/pages/PantryContainer';
 import { ToDoContainer } from './src/pages/ToDoContainer';
@@ -16,18 +18,48 @@ const styles = StyleSheet.create({
 	},
 });
 
-const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function App() {
+	function goSettings() {}
+
 	return (
-		<ThemeProvider>
-			<NavigationContainer>
-				<Stack.Navigator initialRouteName="Groceries">
-					<Stack.Screen name="ToDo" component={ToDoContainer} />
-					<Stack.Screen name="Pantry" component={PantryContainer} />
-					<Stack.Screen name="Groceries" component={GroceryContainer} />
-				</Stack.Navigator>
-			</NavigationContainer>
-		</ThemeProvider>
+		<SafeAreaProvider>
+			<ThemeProvider>
+				<NavigationContainer>
+					<Tab.Navigator
+						initialRouteName="Groceries"
+						screenOptions={({ route }) => ({
+							tabBarIcon: ({ focused, color, size }) => {
+								let iconName = '';
+								switch (route.name) {
+									case 'ToDo':
+										iconName = 'list';
+										break;
+
+									case 'Groceries':
+										iconName = 'basket';
+										break;
+
+									case 'Pantry':
+										iconName = 'home';
+										break;
+
+									default:
+										break;
+								}
+
+								if (!focused) iconName += '-outline';
+
+								return <Ionicons name={iconName} size={size} color={color} />;
+							},
+						})}>
+						<Tab.Screen name="ToDo" component={ToDoContainer} />
+						<Tab.Screen name="Pantry" component={PantryContainer} />
+						<Tab.Screen name="Groceries" component={GroceryContainer} />
+					</Tab.Navigator>
+				</NavigationContainer>
+			</ThemeProvider>
+		</SafeAreaProvider>
 	);
 }
